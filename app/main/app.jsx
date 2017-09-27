cc77a9df246e41da770299baab03c9d355e3c836
Brian Brennan <me@brianbrennan.com>
2017-09-27 16:57:52 -0400

    adding in new component

--- app/main/app.jsx
+++ app/main/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import MainHeader from '../globals/components/MainHeader/MainHeader.jsx';
+
 class App extends React.Component {
 
     render() {
         return (
             <section className="appWrap">
-                <h1>Hello World!</h1>
+                <MainHeader></MainHeader>
             </section>
         );
     }
